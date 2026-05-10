471bbccc0e99bd172bddef70bbdbdb0baf56d02a
Anton <antonms2002@gmail.com>
2026-05-10 16:35:08 +0300

    implemented POM: base_page, main_page, updated tests

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,23 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class BasePage:
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
+        self.wait = WebDriverWait(self.browser, 10)
 
     def open(self):
         self.browser.get(self.url)
+
+    def find_element(self, locator):
+        return self.wait.until(EC.presence_of_element_located(locator))
+
+    def is_element_present(self, locator):
+        try:
+            self.wait.until(EC.presence_of_element_located(locator))
+            return True
+        except NoSuchElementException:
+            return False
